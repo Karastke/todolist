6dc859ea6668fd7c873cdee43b30ffa51bd5ad94
Karastke <fkdlsaos1@naver.com>
2024-04-26 02:37:19 +0900

    feat: add edit feature
    수정 기능을 추가

--- src/assets/components/TodoList.jsx
+++ src/assets/components/TodoList.jsx
@@ -2,6 +2,7 @@ import React, { useState }  from 'react'
 import Form from './Form'
 import {v4 as uuidv4} from 'uuid'
 import Todo from './Todo'
+import Edit from './Edit'
 uuidv4()
 
 
@@ -15,12 +16,27 @@ const TodoList = () => {
   const deleteTodo = id => {
     setTodo(todoValue.filter(todo => todo.id !== id))
   }
+
+  const editTodo = id => {
+    setTodo(todoValue.map(todo => todo.id === id ? {...todo,
+    isEditing:!todo.isEditing}: todo))
+  }
+
+  const editTask = (task, id) => {
+    setTodo(todoValue.map(todo => todo.id === id ? {...todo,
+    task, isEditing: !todo.isEditing} : todo))
+  }
   return (
     <div className='container bg-gray-700 mt-20 p-8 rounded-md'>
         <Form createTodo = {createTodo}/>
         {
           todoValue.map((todo, index) => (
-          <Todo task={todo} key={index} deleteTodo={deleteTodo}/>
+            todo.isEditing ? (
+              <Edit key={index} editTodo={editTask} task={todo}/>
+            ) : (
+              <Todo task={todo} key={index} deleteTodo={deleteTodo}
+              editTodo={editTodo}/>
+            )
         )) 
         }
     </div>
