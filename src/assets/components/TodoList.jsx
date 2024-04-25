6987f62efd69fff154f8c8a4fddc11cffdfebe5d
Karastke <fkdlsaos1@naver.com>
2024-04-26 01:01:41 +0900

    Feat: add useState features
    할 일을 담아둘 수 있는 배열 추가

--- src/assets/components/TodoList.jsx
+++ src/assets/components/TodoList.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import Form from './Form'
+import {v4 as uuidv4} from 'uuid'
+uuidv4()
+
 
 const TodoList = () => {
+  const [todoValue, setTodo] = useState([]);
+
+  const createTodo = todo => {
+    setTodo([...todoValue, {id: uuidv4(), task: todo, isEditing: false}])
+  }
   return (
-    <div>
-        <Form/>
+    <div className='container bg-gray-700 mt-20 p-8 rounded-md'>
+        <Form createTodo = {createTodo}/>
     </div>
   )
 }
