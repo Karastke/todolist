0a63cf9099cd01e54732441f3e0d98cdbb955f1f
Karastke <fkdlsaos1@naver.com>
2024-04-26 02:25:12 +0900

    feat: add delete feature
    할 일 목록 중 삭제 기능 추가

--- src/assets/components/TodoList.jsx
+++ src/assets/components/TodoList.jsx
@@ -11,12 +11,16 @@ const TodoList = () => {
   const createTodo = todo => {
     setTodo([...todoValue, {id: uuidv4(), task: todo, isEditing: false}])
   }
+
+  const deleteTodo = id => {
+    setTodo(todoValue.filter(todo => todo.id !== id))
+  }
   return (
     <div className='container bg-gray-700 mt-20 p-8 rounded-md'>
         <Form createTodo = {createTodo}/>
         {
           todoValue.map((todo, index) => (
-          <Todo task={todo} key={index}/>
+          <Todo task={todo} key={index} deleteTodo={deleteTodo}/>
         )) 
         }
     </div>
